c6b12e7b7e93f7f58e4b40b93aabcbbed142aa1a
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-10-20 16:15:50 -0300

    feat: função para buscar todas as hastags

--- src/controllers/hashtags.controllers.js
+++ src/controllers/hashtags.controllers.js
@@ -1,11 +1,9 @@
 import { STATUS_CODE } from "../enums/status.code.js";
 import * as hashtagsRepository from "../repositories/hashtags.repository.js";
 
-const listPostHashtag = async (req, res) => {
-  const { hashtagName } = req.params;
-
+const listHashtags = async (req, res) => {
   try {
-    const result = await hashtagsRepository.listPostbyHashtag(hashtagName);
+    const result = await hashtagsRepository.listHashtags();
     res.status(STATUS_CODE.OK).send(result.rows);
     return;
   } catch (error) {
@@ -14,4 +12,17 @@ const listPostHashtag = async (req, res) => {
   }
 };
 
-export { listPostHashtag };
+const listPostHashtag = async (req, res) => {
+  const { hashtagName } = req.params;
+
+  try {
+    const result = await hashtagsRepository.listPostbyHashtag(hashtagName);
+    res.status(STATUS_CODE.OK).send(result.rows);
+    return;
+  } catch (error) {
+    res.status(STATUS_CODE.SERVER_ERROR);
+    return;
+  }
+};
+
+export { listPostHashtag, listHashtags };
--- src/repositories/hashtags.repository.js
+++ src/repositories/hashtags.repository.js
@@ -1,6 +1,10 @@
 import { connection } from "../database/db.js";
 import { TABLE } from "../enums/tables.js";
 
+async function listHashtags() {
+  return connection.query(`SELECT * FROM ${TABLE.HASHTAGS};`);
+}
+
 async function listPostbyHashtag(text) {
   const newText = `%${text}%`;
   return connection.query(`SELECT * FROM ${TABLE.POSTS} WHERE text LIKE $1;`, [
@@ -8,4 +12,4 @@ async function listPostbyHashtag(text) {
   ]);
 }
 
-export { listPostbyHashtag };
+export { listPostbyHashtag, listHashtags };
