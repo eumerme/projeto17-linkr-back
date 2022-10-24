c27a0807a6a4fa14b862c2cc763de3e6d95bc1bc
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-10-24 15:30:38 -0300

    fix: função para rastrear hashtags

--- src/controllers/timeline.controllers.js
+++ src/controllers/timeline.controllers.js
@@ -2,98 +2,99 @@ import { STATUS_CODE } from "../enums/status.code.js";
 import * as timelineRepository from "../repositories/timeline.repository.js";
 
 async function publishPost(req, res) {
-	const { userId } = res.locals;
-	const { comment, url } = req.body;
+  const { userId } = res.locals;
+  const { comment, url } = req.body;
 
-	try {
-		await timelineRepository.publishNewPost(userId, comment, url);
-		res.sendStatus(STATUS_CODE.CREATED);
-		return;
-	} catch (error) {
-		res.sendStatus(STATUS_CODE.SERVER_ERROR);
-		return;
-	}
+  try {
+    await timelineRepository.publishNewPost(userId, comment, url);
+    res.sendStatus(STATUS_CODE.CREATED);
+    return;
+  } catch (error) {
+    res.sendStatus(STATUS_CODE.SERVER_ERROR);
+    return;
+  }
 }
 
 const listPosts = async (req, res) => {
-	try {
-		const result = await timelineRepository.listPost();
-		return res.status(STATUS_CODE.OK).send(result.rows);
-	} catch (error) {
-		return res.sendStatus(STATUS_CODE.SERVER_ERROR);
-	}
+  try {
+    const result = await timelineRepository.listPost();
+    return res.status(STATUS_CODE.OK).send(result.rows);
+  } catch (error) {
+    return res.sendStatus(STATUS_CODE.SERVER_ERROR);
+  }
 };
 
 const likes = (req, res) => {
-	const { id, type, userId } = req.body;
+  const { id, type, userId } = req.body;
 
-	try {
-		timelineRepository.updateLikes(id, userId, type);
-		return res.sendStatus(STATUS_CODE.OK);
-	} catch (error) {
-		return res.sendStatus(STATUS_CODE.SERVER_ERROR);
-	}
+  try {
+    timelineRepository.updateLikes(id, userId, type);
+    return res.sendStatus(STATUS_CODE.OK);
+  } catch (error) {
+    return res.sendStatus(STATUS_CODE.SERVER_ERROR);
+  }
 };
 
 const updatePost = async (req, res) => {
-	const { comment } = req.body;
-	const { id } = req.params;
-	try {
-		await timelineRepository.editPostText(comment, id);
-		res.sendStatus(STATUS_CODE.CREATED);
-		return;
-	} catch (error) {
-		return res.sendStatus(STATUS_CODE.SERVER_ERROR);
-	}
+  const { comment } = req.body;
+  const { id } = req.params;
+  try {
+    await timelineRepository.editPostText(comment, id);
+    res.sendStatus(STATUS_CODE.CREATED);
+    return;
+  } catch (error) {
+    return res.sendStatus(STATUS_CODE.SERVER_ERROR);
+  }
 };
 
 const deletePost = async (req, res) => {
-	const { id } = req.params;
-	try {
-		await timelineRepository.deleteFatalPost(id);
-		return res.sendStatus(STATUS_CODE.OK);
-	} catch (error) {
-		return res.sendStatus(STATUS_CODE.SERVER_ERROR);
-	}
+  const { id } = req.params;
+  try {
+    await timelineRepository.deleteFatalPost(id);
+    return res.sendStatus(STATUS_CODE.OK);
+  } catch (error) {
+    console.log(error.message);
+    return res.sendStatus(STATUS_CODE.SERVER_ERROR);
+  }
 };
 
 const listLikes = async (req, res) => {
-	const { id } = req.params;
+  const { id } = req.params;
 
-	try {
-		const result = await timelineRepository.likes(id);
-		return res.status(STATUS_CODE.OK).send(result.rows);
-	} catch (error) {
-		return res.sendStatus(STATUS_CODE.SERVER_ERROR);
-	}
+  try {
+    const result = await timelineRepository.likes(id);
+    return res.status(STATUS_CODE.OK).send(result.rows);
+  } catch (error) {
+    return res.sendStatus(STATUS_CODE.SERVER_ERROR);
+  }
 };
 
 const listUsers = async (req, res) => {
-	try {
-		const { rows: users } = await timelineRepository.getUsers();
-		return res.status(STATUS_CODE.OK).send(users);
-	} catch (error) {
-		return res.sendStatus(STATUS_CODE.SERVER_ERROR);
-	}
+  try {
+    const { rows: users } = await timelineRepository.getUsers();
+    return res.status(STATUS_CODE.OK).send(users);
+  } catch (error) {
+    return res.sendStatus(STATUS_CODE.SERVER_ERROR);
+  }
 };
 
 const listUserPosts = async (req, res) => {
-	const { id } = req.params;
-	try {
-		const { rows: posts } = await timelineRepository.getUserPosts(id);
-		return res.status(STATUS_CODE.OK).send(posts);
-	} catch (error) {
-		return res.sendStatus(STATUS_CODE.SERVER_ERROR);
-	}
+  const { id } = req.params;
+  try {
+    const { rows: posts } = await timelineRepository.getUserPosts(id);
+    return res.status(STATUS_CODE.OK).send(posts);
+  } catch (error) {
+    return res.sendStatus(STATUS_CODE.SERVER_ERROR);
+  }
 };
 
 export {
-	publishPost,
-	listPosts,
-	updatePost,
-	deletePost,
-	likes,
-	listLikes,
-	listUsers,
-	listUserPosts,
+  publishPost,
+  listPosts,
+  updatePost,
+  deletePost,
+  likes,
+  listLikes,
+  listUsers,
+  listUserPosts,
 };
--- src/repositories/hashtags.repository.js
+++ src/repositories/hashtags.repository.js
@@ -2,7 +2,7 @@ import { connection } from "../database/db.js";
 import { TABLE } from "../enums/tables.js";
 
 async function listHashtags() {
-	return connection.query(`
+  return connection.query(`
 	SELECT ${TABLE.HASHTAGS}.name
 		, COUNT("${TABLE.HASHTAGSPOSTS}"."postId") AS amount
 	FROM ${TABLE.HASHTAGS}
@@ -13,9 +13,9 @@ async function listHashtags() {
 }
 
 async function listPostbyHashtag(text) {
-	const newText = `%${text}%`;
-	return connection.query(
-		`SELECT posts.text, 
+  const newText = `%#${text}%`;
+  return connection.query(
+    `SELECT posts.text, 
 			posts.url, 
 			users.name,
 			users."imageUrl" 
@@ -24,82 +24,80 @@ async function listPostbyHashtag(text) {
 		WHERE text LIKE $1 
 		ORDER BY posts."createdAt" DESC 
 		LIMIT 20;`,
-		[newText]
-	);
+    [newText]
+  );
 }
 
 async function insertHashtag(hashtag) {
-	return connection.query(`INSERT INTO ${TABLE.HASHTAGS} (name) VALUES ($1);`, [
-		hashtag,
-	]);
+  return connection.query(`INSERT INTO ${TABLE.HASHTAGS} (name) VALUES ($1);`, [
+    hashtag,
+  ]);
 }
 
 async function selectHashtag(hashtag) {
-	return connection.query(
-		`SELECT id AS "hashtagId" FROM ${TABLE.HASHTAGS} WHERE name = $1;`,
-		[hashtag]
-	);
+  return connection.query(
+    `SELECT id AS "hashtagId" FROM ${TABLE.HASHTAGS} WHERE name = $1;`,
+    [hashtag]
+  );
 }
 
 async function selectPostId(id) {
-	return connection.query(
-		`SELECT id AS "postId" FROM ${TABLE.POSTS} where "userId" = $1 ORDER BY "createdAt" DESC;`,
-		[id]
-	);
+  return connection.query(
+    `SELECT id AS "postId" FROM ${TABLE.POSTS} where "userId" = $1 ORDER BY "createdAt" DESC;`,
+    [id]
+  );
 }
 
 async function insertHashtagsPosts(postId, hashtagId) {
-	return connection.query(
-		`INSERT INTO "${TABLE.HASHTAGSPOSTS}" ("postId", "hashtagId") VALUES ($1, $2);`,
-		[postId, hashtagId]
-	);
+  return connection.query(
+    `INSERT INTO "${TABLE.HASHTAGSPOSTS}" ("postId", "hashtagId") VALUES ($1, $2);`,
+    [postId, hashtagId]
+  );
 }
 
 async function selectHashtagName(id) {
-	console.log("select is post", id);
-	return connection.query(
-		`SELECT ${TABLE.HASHTAGS}.name
+  return connection.query(
+    `SELECT ${TABLE.HASHTAGS}.name
 		FROM "${TABLE.HASHTAGSPOSTS}"
 		JOIN ${TABLE.POSTS} ON ${TABLE.POSTS}.id = "${TABLE.HASHTAGSPOSTS}"."postId"
 		JOIN ${TABLE.HASHTAGS} ON ${TABLE.HASHTAGS}.id = "${TABLE.HASHTAGSPOSTS}"."hashtagId"
 		WHERE ${TABLE.POSTS}.id = $1;`,
-		[id]
-	);
+    [id]
+  );
 }
 
 async function selectHashtagsPosts(hashtag) {
-	console.log("select ", hashtag);
-	return connection.query(
-		`SELECT ${TABLE.HASHTAGS}.name
+  return connection.query(
+    `SELECT ${TABLE.HASHTAGS}.name
 			, COUNT("${TABLE.HASHTAGSPOSTS}"."postId") AS amount
 		FROM "${TABLE.HASHTAGSPOSTS}"
 		JOIN ${TABLE.POSTS} ON ${TABLE.POSTS}.id = "${TABLE.HASHTAGSPOSTS}"."postId"
 		JOIN ${TABLE.HASHTAGS} ON ${TABLE.HASHTAGS}.id = "${TABLE.HASHTAGSPOSTS}"."hashtagId"
 		WHERE ${TABLE.HASHTAGS}.name = $1
 		GROUP BY ${TABLE.HASHTAGS}.name;`,
-		[hashtag]
-	);
+    [hashtag]
+  );
 }
 
 async function deleteHashtagsPosts(id) {
-	return connection.query(`DELETE FROM "hashtagsPosts" WHERE "postId" = $1;`, [
-		id,
-	]);
+  return connection.query(`DELETE FROM "hashtagsPosts" WHERE "postId" = $1;`, [
+    id,
+  ]);
 }
 
 async function deleteHashtags(hashtag) {
-	return connection.query(`DELETE FROM hashtags WHERE name = $1;`, [hashtag]);
+  return connection.query(`DELETE FROM hashtags WHERE name = $1;`, [hashtag]);
 }
 
 export {
-	listPostbyHashtag,
-	listHashtags,
-	insertHashtag,
-	selectHashtag,
-	selectPostId,
-	insertHashtagsPosts,
-	selectHashtagsPosts,
-	selectHashtagName,
-	deleteHashtagsPosts,
-	deleteHashtags,
+  listPostbyHashtag,
+  listHashtags,
+  insertHashtag,
+  selectHashtag,
+  selectPostId,
+  insertHashtagsPosts,
+  selectHashtagsPosts,
+  selectHashtagName,
+  deleteHashtagsPosts,
+  deleteHashtags,
 };
