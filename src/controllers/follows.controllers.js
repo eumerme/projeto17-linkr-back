686e73ca33cd9c211ccc482dc25d3cc328894c37
eumerme <euthaisetau@gmail.com>
2022-10-25 11:38:47 -0300

    feat: função de follow e unfollow pelo clique do usuário

--- src/controllers/follows.controllers.js
+++ src/controllers/follows.controllers.js
@@ -1,8 +1,9 @@
 import { STATUS_CODE } from "../enums/status.code.js";
 import * as followsRepository from "../repositories/follows.repository.js";
 
-const isFollowing = async (req, res) => {
+const follows = async (req, res) => {
 	const { userId, followeeId } = req.body;
+	const { path } = req.route;
 
 	if (!userId || !followeeId) {
 		return res.sendStatus(STATUS_CODE.UNPROCESSABLE_ENTITY);
@@ -14,16 +15,27 @@ const isFollowing = async (req, res) => {
 			followeeId
 		);
 
-		if (result.length !== 0) {
-			return res.status(STATUS_CODE.OK).send({ follows: true });
-		} else {
-			return res.status(STATUS_CODE.OK).send({ follows: false });
+		if (path === "/is-following") {
+			if (result.length !== 0) {
+				return res.status(STATUS_CODE.OK).send({ follows: true });
+			} else {
+				return res.status(STATUS_CODE.OK).send({ follows: false });
+			}
 		}
-		console.log(result);
+
+		if (path === "/follow-unfollow") {
+			if (result.length !== 0) {
+				await followsRepository.unfollow(userId, followeeId);
+				return res.status(STATUS_CODE.OK).send({ follows: false });
+			} else {
+				await followsRepository.follow(userId, followeeId);
+				return res.status(STATUS_CODE.OK).send({ follows: true });
+			}
+		}
 	} catch (error) {
 		console.log(error.message);
 		return res.status(STATUS_CODE.SERVER_ERROR).send(error.message);
 	}
 };
 
-export { isFollowing };
+export { follows };
--- src/repositories/follows.repository.js
+++ src/repositories/follows.repository.js
@@ -8,4 +8,18 @@ async function selectFollower(userId, followeeId) {
 	);
 }
 
-export { selectFollower };
+async function follow(userId, followeeId) {
+	return connection.query(
+		`INSERT INTO follows ("userId", "followeeId") VALUES ($1, $2);`,
+		[userId, followeeId]
+	);
+}
+
+async function unfollow(userId, followeeId) {
+	return connection.query(
+		`DELETE FROM follows WHERE "userId" = $1 AND "followeeId" = $2;`,
+		[userId, followeeId]
+	);
+}
+
+export { selectFollower, follow, unfollow };
