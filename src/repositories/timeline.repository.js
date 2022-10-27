1ab9ff08197417fd5ae52a7cc848021573641a57
Amorim <matheusnavesamorim@gmail.com>
2022-10-27 18:34:01 -0300

    Feat: adicionando reposts na timeline

--- src/repositories/timeline.repository.js
+++ src/repositories/timeline.repository.js
@@ -132,9 +132,27 @@ async function listUserNotFollowing() {
 }
 
 async function createNewRepost(postId, userId){
-  return connection.query(
-    `INSERT INTO ${TABLE.REPOSTS} ("userId", "postId") VALUES ($1, $2)`
+
+  await connection.query(
+    `INSERT INTO ${TABLE.REPOSTS} ("userId", "postId") VALUES ($1, $2);`
   , [userId, postId]);
+
+  const result = (await connection.query(`
+    SELECT reposts.id AS "repostId",
+    reposts."userId",
+    posts.id,
+    posts.text,
+    posts.url,
+    users.name
+    FROM reposts 
+    JOIN posts ON posts.id=reposts."postId"
+    JOIN users on posts."userId"=users.id
+    WHERE reposts.id = (SELECT MAX(reposts.id) FROM reposts);
+  `)).rows[0];
+  
+  connection.query(`
+    INSERT INTO ${TABLE.POSTS} ("userId", text, url, "repostBy") VALUES ($1, $2, $3, $4);`
+  , [result.userId, result.text, result.url, result.repostId]);
 };
 
 async function countReposts(id){
